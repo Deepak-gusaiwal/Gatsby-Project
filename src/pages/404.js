d33f436165099c3388c7956c60b95b5b9c333ff9
Deepak <dgusaiwal537@Gmail.com>
2023-02-02 13:20:41 +0530

    Adding 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button, HeadingMedium, Wrapper } from '../styles/GlobalStyledComponents'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const ErrorPage = () => {
+  return (
+   <Wrapper>
+    <Container>
+    <HeadingMedium>404 NOT FOUND</HeadingMedium>
+    <Button large as={Link}>Go Back</Button>
+    </Container>
+   </Wrapper>
+  )
+}
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+min-height: 60vh;
+flex-direction: column;
+`
+
+export default ErrorPage
